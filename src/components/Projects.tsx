43dbe3ce4f1fad5d52165e37b48b27b389838825
itsPk99 <singhpradum27121999@gmail.com>
2025-05-29 17:24:57 +0530

    Second commit

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,8 +15,8 @@ const Projects = () => {
       image: 'https://images.unsplash.com/photo-1467232004584-a241de8bcf5d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80',
       category: 'design',
       technologies: ['React', 'Tailwind CSS', 'Framer Motion'],
-      liveLink: 'https://example.com',
-      githubLink: 'https://github.com',
+      liveLink: 'https://portfolio-six-blond-bq16avalyz.vercel.app/',
+      githubLink: 'https://github.com/Sonukumar420/Portfolio',
     },
     {
       id: 2,
